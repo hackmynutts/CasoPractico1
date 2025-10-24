60ff1a2ceed18f2ba94fc1927f6fa2035f0654ea
hackmynutts <jorgemoruagonzalez@gmail.com>
2025-10-23 20:58:02 -0600

    Reserve process, adding reservation with monto total being calculated. adding by id room. list of available rooms, pending change estado to false, when room gets booked...

---
+++ Controllers/ReserveController.cs
@@ -0,0 +1,98 @@
+﻿using CasoPractico1_JorgeMorua.Abstractions.BusinessLogic.Reservations.AddReservation;
+using CasoPractico1_JorgeMorua.Abstractions.BusinessLogic.Reserve.ReserveList;
+using CasoPractico1_JorgeMorua.Abstractions.BusinessLogic.Rooms.GetRoom;
+using CasoPractico1_JorgeMorua.Abstractions.UIModules.Reservations;
+using CasoPractico1_JorgeMorua.Abstractions.UIModules.Rooms;
+using CasoPractico1_JorgeMorua.BusinessLogic.Reservations.AddReservation;
+using CasoPractico1_JorgeMorua.BusinessLogic.Reserve.ReserveList;
+using CasoPractico1_JorgeMorua.BusinessLogic.Rooms.GetRoom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasoPractico1_JorgeMorua.Controllers
+{
+    public class ReserveController : Controller
+    {
+        private readonly IReserveList_BL _reserveListBL;
+        private readonly IAddReservation_BL _addReservation_BL;
+        private readonly IGetRoom_BL _getRoom_BL;
+        public ReserveController() 
+        {
+            _reserveListBL = new ReserveList_BL();
+            _addReservation_BL = new AddReservation_BL();
+            _getRoom_BL = new GetRoom_BL();
+        }
+        // GET: Reserve
+        public ActionResult ReserveList()
+        {
+            List<RoomsDTO> availableRooms = _reserveListBL.GetReserveList();
+            return View(availableRooms);
+        }
+
+        // GET: Reserve/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reserve/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reserve/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reserve/Reserve/5
+        public ActionResult Reserve(int id)
+        {
+            RoomsDTO roomSelected = _getRoom_BL.GetRoomID(id);
+            ReservationsDTO reserve = new ReservationsDTO
+            {
+                idRoom = roomSelected.id
+            };
+            return View("~/Views/Reservations/Create.cshtml", reserve);
+        }
+
+
+        // GET: Reserve/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reserve/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
